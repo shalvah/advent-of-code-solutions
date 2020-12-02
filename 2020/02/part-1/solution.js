e3ec362645fe18e0e7964c4e2f9beb1d8eddc10b
Shalvah <shalvah@users.noreply.github.com>
2020-12-02 11:47:15 +0100

    Create solution.js

---
+++ 2020/02/part-1/solution.js
@@ -0,0 +1,22 @@
+const rl = require('readline').createInterface({
+    input: require('fs').createReadStream('test.txt'),
+    crlfDelay: Infinity
+});
+
+async function find() {
+    let valid = 0;
+    for await (const l of rl) {
+        let [number, letter, password] = l.split(' ');
+        let [lower, upper] = number.split("-");
+        letter = letter[0];
+        const occurrences = (password.match(new RegExp(letter, "g")) || []).length;
+        if (lower <= occurrences && occurrences <= upper) {
+            console.log({ number, letter, password });
+            valid++;
+        }
+    }
+    return valid;
+}
+
+
+find().then(console.log);
