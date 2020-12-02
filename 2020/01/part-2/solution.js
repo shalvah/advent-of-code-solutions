807693f96691082b040f29f231bb7f5b89188eae
Shalvah <shalvah@users.noreply.github.com>
2020-12-02 01:10:25 +0100

    Update solution.js

--- 2020/01/part-2/solution.js
+++ 2020/01/part-2/solution.js
@@ -1,21 +1,26 @@
-const rl = require('readline').createInterface({
-    input: require('fs').createReadStream('input.txt'), // same input as Part 1
+const rl1 = require('readline').createInterface({
+    input: require('fs').createReadStream('test.txt'),
     crlfDelay: Infinity
 });
-const s = new Set;
+const r = new Map;
 
 async function find() {
-    for await (let n of rl) {
+    for await (let n of rl1) {
         if (n = n.trim()) {
-            s.add(n);
             const complement1 = 2020 - parseInt(n);
-            for (let m of s) {
+            for (let m of r.keys()) {
                 const complement2 = complement1 - parseInt(m);
-                if (s.has(complement2.toString())) {
+                let occurrences = r.get(complement2.toString());
+                if (
+                    (complement2 == parseInt(n) && occurrences > 1)
+                    || (complement2 == parseInt(m) && occurrences > 1)
+                    || (!([m, n].includes(`${complement2}`)) && occurrences == 1)
+                ) {
                     console.log({ n, m, complement2 });
                     return n * m * complement2;
                 }
             }
+            r.set(n, (r.get(n) || 0) + 1);
         }
     }
 }
