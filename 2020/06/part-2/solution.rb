83344871788302d2bea2eec4a78b9b15b2cb6997
Shalvah <shalvah@users.noreply.github.com>
2020-12-06 16:06:14 +0100

    Create solution.rb

---
+++ 2020/06/part-2/solution.rb
@@ -0,0 +1,52 @@
+=begin
+--- Part Two ---
+As you finish the last group's customs declaration, you notice that you misread one word in the instructions:
+
+You don't need to identify the questions to which anyone answered "yes"; you need to identify the questions to which everyone answered "yes"!
+
+Using the same example as above:
+
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+This list represents answers from five groups:
+
+In the first group, everyone (all 1 person) answered "yes" to 3 questions: a, b, and c.
+In the second group, there is no question to which everyone answered "yes".
+In the third group, everyone answered yes to only 1 question, a. Since some people did not answer "yes" to b or c, they don't count.
+In the fourth group, everyone answered yes to only 1 question, a.
+In the fifth group, everyone (all 1 person) answered "yes" to 1 question, b.
+In this example, the sum of these counts is 3 + 0 + 1 + 1 + 1 = 6.
+
+For each group, count the number of questions to which everyone answered "yes". What is the sum of those counts?
+=end
+
+file = File.open("input.txt")
+input = file.read.split("\n\n")
+
+count = 0
+input.each do |group|
+  answered = {}
+  persons = group.split
+  persons.each do |person|
+    person.each_char do |question|
+      answered[question] = (answered[question] || 0) + 1
+    end
+  end
+  answered_by_all = answered.select { |question, answers| answers === persons.size }
+  count += answered_by_all.size
+end
+
+p count
