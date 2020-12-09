8e48132055ebd6bc60b616de8b5efa76913a6d01
Shalvah <shalvah@users.noreply.github.com>
2020-12-09 13:55:44 +0100

    Update intcode.rb

--- 2019/09/intcode.rb
+++ 2019/09/intcode.rb
@@ -1,13 +1,13 @@
 class Intcode
-  attr_writer :get_input, :send_output
-
-  def initialize(input)
+  def initialize(input, get_input, send_output)
     @input = input
+    @get_input = get_input
+    @send_output = send_output
     @relative_base = 0
   end
 
   def get_opcode_and_parameter_modes(instruction)
-    opcode = (instruction[-2..-1] || instruction[0]).to_i # The instruction is supplied as 1 - 4 digits
+    opcode = (instruction[-2..-1] || instruction[0]).to_i # The instruction is supplied as 1 - 5 digits
     p1_mode = instruction[-3] || '0'
     p2_mode = instruction[-4] || '0'
     p3_mode = instruction[-5] || '0'
@@ -17,7 +17,7 @@ class Intcode
   def get_value_with_mode(parameter, parameter_mode)
     case parameter_mode
     when '0' # Position mode
-      @input[parameter].to_i # Any memory beyond bounds will return 0
+      @input[parameter].to_i # Any memory beyond bounds will return nil, which to_i makes 0, so we're good
     when '1' # Immediate mode
       parameter
     when '2' # Relative mode
@@ -40,25 +40,25 @@ class Intcode
       opcode, p1_mode, p2_mode, p3_mode = get_opcode_and_parameter_modes(@input[i])
 
       case opcode
-      when 99
+      when 99 # End
         return @input.join(',')
-      when 1
+      when 1 # Add
         val1 = get_value_with_mode(@input[i + 1].to_i, p1_mode)
         val2 = get_value_with_mode(@input[i + 2].to_i, p2_mode)
         result_position = get_output_address_with_mode(@input[i + 3].to_i, p3_mode)
         @input[result_position] = (val1 + val2).to_s
         i += 4
-      when 2
+      when 2 # Multiply
         val1 = get_value_with_mode(@input[i + 1].to_i, p1_mode)
         val2 = get_value_with_mode(@input[i + 2].to_i, p2_mode)
         result_position = get_output_address_with_mode(@input[i + 3].to_i, p3_mode)
         @input[result_position] = (val1 * val2).to_s
         i += 4
-      when 3 # Get input
+      when 3 # Get input and store
         data_position = get_output_address_with_mode(@input[i + 1].to_i, p1_mode)
         @input[data_position] = @get_input.call
         i += 2
-      when 4
+      when 4 # Send output
         val = get_value_with_mode(@input[i + 1].to_i, p1_mode)
         @send_output.call(val)
         i += 2
