2968ff3235c550a38484a68c9904e4a1e0997dfb
Shalvah <shalvah@users.noreply.github.com>
2020-12-15 22:23:25 +0100

    Update part-2.rb

--- 2020/15/part-2.rb
+++ 2020/15/part-2.rb
@@ -15,24 +15,31 @@ Given your starting numbers, what will be the 30000000th number spoken?
 input = [1,17,0,10,18,11,6]
 
 spoken_numbers = {}
-sequence = []
+current_number = []
 
+# Same solution as Part 1; just change n
 n = 30000000
 n.times do |i|
   if i < input.size
-    number = input[i]
+    current_number = input[i]
   else
-    last_number_spoken = sequence.last
+    last_number_spoken = current_number
     if spoken_numbers[last_number_spoken] != nil
       last_spoken = spoken_numbers[last_number_spoken].last
       if spoken_numbers[last_number_spoken].size == 1
-        number = 0
+        current_number = 0
       else
         last_spoken_before_that = spoken_numbers[last_number_spoken][-2]
-        number = last_spoken - last_spoken_before_that
+        current_number = last_spoken - last_spoken_before_that
       end
     end
   end
+
+  spoken_numbers[current_number] ||= []
+  spoken_numbers[current_number] << i
+end
+
+p current_number
 
   sequence << number
   spoken_numbers[number] ||= []
