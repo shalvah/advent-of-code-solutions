850e9d89d6536855c4e033dac3e88764acd84227
Shalvah <shalvah@users.noreply.github.com>
2020-12-15 22:00:18 +0100

    Create part-2.rb

---
+++ 2020/15/part-2.rb
@@ -0,0 +1,42 @@
+=begin
+--- Part Two ---
+Impressed, the Elves issue you a challenge: determine the 30000000th number spoken. For example, given the same starting numbers as above:
+
+Given 0,3,6, the 30000000th number spoken is 175594.
+Given 1,3,2, the 30000000th number spoken is 2578.
+Given 2,1,3, the 30000000th number spoken is 3544142.
+Given 1,2,3, the 30000000th number spoken is 261214.
+Given 2,3,1, the 30000000th number spoken is 6895259.
+Given 3,2,1, the 30000000th number spoken is 18.
+Given 3,1,2, the 30000000th number spoken is 362.
+Given your starting numbers, what will be the 30000000th number spoken?
+=end
+
+input = [1,17,0,10,18,11,6]
+
+spoken_numbers = {}
+sequence = []
+
+n = 30000000
+n.times do |i|
+  if i < input.size
+    number = input[i]
+  else
+    last_number_spoken = sequence.last
+    if spoken_numbers[last_number_spoken] != nil
+      last_spoken = spoken_numbers[last_number_spoken].last
+      if spoken_numbers[last_number_spoken].size == 1
+        number = 0
+      else
+        last_spoken_before_that = spoken_numbers[last_number_spoken][-2]
+        number = last_spoken - last_spoken_before_that
+      end
+    end
+  end
+
+  sequence << number
+  spoken_numbers[number] ||= []
+  spoken_numbers[number] << i
+end
+
+p sequence.last
