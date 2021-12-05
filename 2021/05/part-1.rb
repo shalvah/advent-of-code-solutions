14714a6eb58ff5ea97ccd83570186cb9ec5664f9
shalvah <diakon.ng@gmail.com>
2021-12-05 07:18:15 +0100

    2021 Day 5

---
+++ 2021/05/part-1.rb
@@ -0,0 +1,21 @@
+input = File.read(File.join(__dir__, "input.txt")).split("\n").map do |line|
+  line.split(' -> ').map { |coords| coords.split(',').map(&:to_i) }.flatten
+end
+
+$covered = {}
+def mark_covered(x, y)
+  $covered["#{x},#{y}"] ||= 0
+  $covered["#{x},#{y}"] += 1
+end
+
+input.each do |(x1, y1, x2, y2)|
+  if y1 == y2
+    range = (x2 > x1) ? (x1..x2) : (x2..x1)
+    range.each { |x| mark_covered(x, y1) }
+  elsif x1 == x2
+    range = (y2 > y1) ? (y1..y2) : (y2..y1)
+    range.each { |y| mark_covered(x1, y) }
+  end
+end
+
+p $covered.filter { |_, lines_count| lines_count >= 2 }.size
---
+++ 2021/05/part-2.rb
@@ -0,0 +1,31 @@
+input = File.read(File.join(__dir__, "input.txt")).split("\n").map do |line|
+  line.split(' -> ').map { |coords| coords.split(',').map(&:to_i) }.flatten
+end
+
+$covered = {}
+def mark_covered(x, y)
+  $covered["#{x},#{y}"] ||= 0
+  $covered["#{x},#{y}"] += 1
+end
+
+input.each do |(x1, y1, x2, y2)|
+  if y1 == y2
+    range = (x2 > x1) ? (x1..x2) : (x2..x1)
+    range.each { |x| mark_covered(x, y1) }
+  elsif x1 == x2
+    range = (y2 > y1) ? (y1..y2) : (y2..y1)
+    range.each { |y| mark_covered(x1, y) }
+  else
+    m = ((y2 - y1) / (x2 - x1)).abs
+
+    x1, y1, x2, y2 = [x2, y2, x1, y1] if x2 < x1
+    x, y = x1, y1
+    until [x, y] == [x2, y2] do
+      mark_covered(x, y)
+      x, y = x + m, y2 > y1 ? y + m : y - m
+    end
+    mark_covered(x2, y2)
+  end
+end
+
+p $covered.filter { |_, lines_count| lines_count >= 2 }.size
