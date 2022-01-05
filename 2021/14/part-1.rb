885175ee30f11abdc8b71db8caa172dce8fc9708
shalvah <diakon.ng@gmail.com>
2022-01-05 20:45:01 +0100

    More refactorings

--- 2021/14/part-1.rb
+++ 2021/14/part-1.rb
@@ -2,17 +2,15 @@ template, _, *rules = File.read(File.join(__dir__, "input.txt")).split("\n")
 rules = rules.map { |l| l.split(" -> ") }.each_with_object({}) { |rule, all| all[rule[0]] = rule[1] }
 
 state = template.split("")
+first_char = state[0]
 
 10.times do
-  next_state = []
-  next_state.push(state[0])
-  state.each_cons(2) do |pair|
+  state = state.each_cons(2).flat_map do |pair|
     insertion = rules[pair.join("")]
-    next_state.push(insertion) if insertion
-    next_state.push(pair[1])
+    [insertion, pair[1]]
   end
-
-  state = next_state
+  # To avoid duplicates, we only added the second letter of each pair, so we need to add the first char back
+  state.prepend first_char
 end
 
 tally = state.tally
--- 2021/14/part-2.rb
+++ 2021/14/part-2.rb
@@ -1,24 +1,20 @@
 template, _, *rules = File.read(File.join(__dir__, "input.txt")).split("\n")
 $rules = rules.map { |l| l.split(" -> ") }.each_with_object({}) { |rule, all| all[rule[0]] = rule[1] }
 
-pairs = template.split("").each_cons(2).map(&:itself).tally
+# What matters isn't the string itself, but the pairs in the string 😉
+pairs = template.split("").each_cons(2).tally
 40.times do
-  next_pairs = {}
-  pairs.each do |(a, b), freq|
-    if (insertion = $rules[a + b])
-      next_pairs[[a, insertion]] = (next_pairs[[a, insertion]] || 0) + freq
-      next_pairs[[insertion, b]] = (next_pairs[[insertion, b]] || 0) + freq
-    end
+  pairs = pairs.each_with_object({}) do |((a, b), freq), next_pairs|
+    insertion = $rules[a + b]
+    next_pairs[[a, insertion]] = (next_pairs[[a, insertion]] || 0) + freq
+    next_pairs[[insertion, b]] = (next_pairs[[insertion, b]] || 0) + freq
   end
-  pairs = next_pairs
 end
 
-character_counts = {}
-pairs.each do |(a, b), freq|
-  # Only count the first char per pair, to avoid counting chars twice
-  character_counts[a] ||= 0
-  character_counts[a] += freq
+character_counts = pairs.each_with_object({}) do |((a, b), freq), counts|
+  # Only count the last char per pair, to avoid counting chars twice
+  counts[b] = (counts[b] || 0) + freq
 end
-character_counts[template[-1]] += 1 # Then add the char at the end
+character_counts[template[0]] += 1 # Then add the char at the start
 
 p character_counts.values.max - character_counts.values.min
