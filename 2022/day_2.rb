a3c826b0e793d7031a09f7c1af284bf4fb9f3692
Shalvah <shalvah@users.noreply.github.com>
2022-12-02 15:35:19 +0100

    Update day_2.rb

--- 2022/day_2.rb
+++ 2022/day_2.rb
@@ -1,66 +1,57 @@
 input = File.read('day_2.txt')
 
-$scores = { "rock" => 1, "paper" => 2, "scissors" => 3 }
-$result_scores = { "lose" => 0, "draw" => 3, "win" => 6 }
+scores = { "rock" => 1, "paper" => 2, "scissors" => 3 }
+result_scores = { "lose" => 0, "draw" => 3, "win" => 6 }
 
 def play(them, me)
-  case [me, them]
+  case [them, me]
     in ["scissors", "paper"] | ["paper", "rock"] | ["rock", "scissors"]
-      $result_scores["win"]
+      "lose"
     in ["rock", "paper"] | ["paper", "scissors"] | ["scissors", "rock"]
-      $result_scores["lose"]
-    in [same_play, ^same_play] # Or we could use `else`
-      $result_scores["draw"]
+      "win"
+    in [same_move, ^same_move] # Or we could use `else`
+      "draw"
   end
 end
 
 # Just for easier readability
 transform = lambda do |char|
   {
-    "A" => "rock",
-    "B" => "paper",
-    "C" => "scissors",
-    "X" => "rock",
-    "Y" => "paper",
-    "Z" => "scissors",
+    "A" => "rock", "B" => "paper", "C" => "scissors",
+    "X" => "rock", "Y" => "paper", "Z" => "scissors",
   }[char]
 end
 
 all_rounds = input.each_line.map do |round|
-  moves = round.split(" ").map(&transform)
-  $scores[moves[1]] + play(*moves)
+  their_move, my_move = round.split(" ").map(&transform)
+  scores[my_move] + result_scores[play(their_move, my_move)]
 end
 
 # Part 1
 p all_rounds.sum
 
 
-def find_play(them, result)
+def get_move(them, result)
   case [them, result]
     in ["paper", "lose"] | ["scissors", "win"] | ["rock", "draw"]
-      $scores["rock"]
+      "rock"
     in ["scissors", "lose"] | ["rock", "win"] | ["paper", "draw"]
-      $scores["paper"]
+      "paper"
     in ["rock", "lose"] | ["paper", "win"] | ["scissors", "draw"]
-      $scores["scissors"]
+      "scissors"
   end
 end
 
-
 transform = lambda do |char|
   {
-    "A" => "rock",
-    "B" => "paper",
-    "C" => "scissors",
-    "X" => "lose",
-    "Y" => "draw",
-    "Z" => "win",
+    "A" => "rock", "B" => "paper", "C" => "scissors",
+    "X" => "lose", "Y" => "draw", "Z" => "win",
   }[char]
 end
 
 all_rounds = input.each_line.map do |round|
   their_move, result = round.split(" ").map(&transform)
-  $result_scores[result] + find_play(their_move, result)
+  result_scores[result] + scores[get_move(their_move, result)]
 end
 
 # Part 2
