c9f0870a47c499e59a6d05525d3cb7198f213cd7
Shalvah <shalvah@users.noreply.github.com>
2022-12-02 15:37:28 +0100

    Update day_2.rb

--- 2022/day_2.rb
+++ 2022/day_2.rb
@@ -1,24 +1,24 @@
 input = File.read('day_2.txt')
 
-scores = { "rock" => 1, "paper" => 2, "scissors" => 3 }
-result_scores = { "lose" => 0, "draw" => 3, "win" => 6 }
+scores = { :rock => 1, :paper => 2, :scissors => 3 }
+result_scores = { :lose => 0, :draw => 3, :win => 6 }
 
 def play(them, me)
   case [them, me]
-    in ["scissors", "paper"] | ["paper", "rock"] | ["rock", "scissors"]
-      "lose"
-    in ["rock", "paper"] | ["paper", "scissors"] | ["scissors", "rock"]
-      "win"
+    in [:scissors, :paper] | [:paper, :rock] | [:rock, :scissors]
+      :lose
+    in [:rock, :paper] | [:paper, :scissors] | [:scissors, :rock]
+      :win
     in [same_move, ^same_move] # Or we could use `else`
-      "draw"
+      :draw
   end
 end
 
 # Just for easier readability
 transform = lambda do |char|
   {
-    "A" => "rock", "B" => "paper", "C" => "scissors",
-    "X" => "rock", "Y" => "paper", "Z" => "scissors",
+    "A" => :rock, "B" => :paper, "C" => :scissors,
+    "X" => :rock, "Y" => :paper, "Z" => :scissors,
   }[char]
 end
 
@@ -33,19 +33,19 @@ p all_rounds.sum
 
 def get_move(them, result)
   case [them, result]
-    in ["paper", "lose"] | ["scissors", "win"] | ["rock", "draw"]
-      "rock"
-    in ["scissors", "lose"] | ["rock", "win"] | ["paper", "draw"]
-      "paper"
-    in ["rock", "lose"] | ["paper", "win"] | ["scissors", "draw"]
-      "scissors"
+    in [:paper, :lose] | [:scissors, :win] | [:rock, :draw]
+      :rock
+    in [:scissors, :lose] | [:rock, :win] | [:paper, :draw]
+      :paper
+    in [:rock, :lose] | [:paper, :win] | [:scissors, :draw]
+      :scissors
   end
 end
 
 transform = lambda do |char|
   {
-    "A" => "rock", "B" => "paper", "C" => "scissors",
-    "X" => "lose", "Y" => "draw", "Z" => "win",
+    "A" => :rock, "B" => :paper, "C" => :scissors,
+    "X" => :lose, "Y" => :draw, "Z" => :win,
   }[char]
 end
 
