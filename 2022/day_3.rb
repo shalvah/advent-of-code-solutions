3c290af300690540bc4a4f63b0c66153f92418b4
Shalvah <shalvah@users.noreply.github.com>
2022-12-04 09:21:36 +0100

    Update day_3.rb

--- 2022/day_3.rb
+++ 2022/day_3.rb
@@ -11,19 +11,19 @@ end
 
 require 'set'
 
+# Part 1
 duplicate_items = input.each_line.flat_map do |rucksack|
   rucksack.chomp.split("").each_slice(rucksack.size/2).map(&Set.method(:new))
     .reduce(&:intersection).to_a
 end
 
-# Part 1
 p duplicate_items.map(&item_to_priority).sum
 
 
+# Part 2
 common_items = input.split.each_slice(3).flat_map do |rucksacks|
   rucksacks.map { Set.new(_1.split("")) }
     .reduce(&:intersection).to_a
 end
 
-# Part 2
 p common_items.map(&item_to_priority).sum
